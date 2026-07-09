51b3f994d35db0e77843c2f9515a859d336a5e88
ShiraSil <shirasilbermann@gmail.com>
2026-07-10 02:27:01 +0300

    Refactor: add router basename for GitHub Pages and enable React.StrictMode

--- client/src/main.jsx
+++ client/src/main.jsx
@@ -1,9 +1,12 @@
-import { createRoot } from "react-dom/client";
-import App from "./components/App.jsx";
 import React from "react";
+import { createRoot } from "react-dom/client";
 import { BrowserRouter } from "react-router-dom";
+import App from "./components/App.jsx";
+
 createRoot(document.getElementById("root")).render(
-  <BrowserRouter>
-    <App />
-  </BrowserRouter>,
+  <React.StrictMode>
+    <BrowserRouter basename="/react-users-management-system">
+      <App />
+    </BrowserRouter>
+  </React.StrictMode>,
 );
